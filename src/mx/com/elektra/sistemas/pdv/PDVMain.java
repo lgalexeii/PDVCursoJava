c06def137a623f2f5eb3301d9200e34a0e121da8
171127 Grupo Salinas <171127 Grupo Salinas@Lap_22>
2017-11-29 08:06:08 -0600

    PDV project init

---
+++ src/mx/com/elektra/sistemas/pdv/PDVMain.java
@@ -0,0 +1,29 @@
+package mx.com.elektra.sistemas.pdv;
+
+public class PDVMain {
+
+    public static void main(String[] args) {
+        // say a welcome message
+        System.out.println("Bienvenido al sistema Punto de Venta");
+        //show selling items
+        
+        //add items to cart
+        
+        //show items in cart
+        
+        //delete items from cart
+        
+        //purchase items from cart
+            //generate order id
+            
+        //generate and print ticket
+            //tax calculation (IVA 16%)
+            //discount (item, quantity, amount)
+        //invoice
+            //search client by RFC
+            //create a customer
+            //print invoice
+        //close session or to buy again
+    }
+    
+}
