f4d2adf8bf4a24ab8fcc827891ca4a3108dc0bfc
171127 Grupo Salinas <171127 Grupo Salinas@Lap_22>
2017-12-01 09:01:15 -0600

    PDV project main menu changed loop

--- src/mx/com/elektra/sistemas/pdv/PDVMain.java
+++ src/mx/com/elektra/sistemas/pdv/PDVMain.java
@@ -1,7 +1,6 @@
 package mx.com.elektra.sistemas.pdv;
 import java.io.IOException;
 
-import mx.com.elektra.sistemas.itemmgmt.Articulo;
 import mx.com.elektra.sistemas.itemmgmt.Carrito;
 import mx.com.elektra.sistemas.busmgmt.InventarioTienda;
 import mx.com.elektra.sistemas.busmgmt.Tienda;
@@ -20,8 +19,30 @@ public class PDVMain {
         miniAlx.setNombre(".:Mini Super Alex:.");
         System.out.println(miniAlx.getNombre());
         Carrito carrito=null;
-
-        mainMenu(carrito,miniAlx);
+        boolean continuar = true;
+        //menu principal
+        do{
+             System.out.println("...::| Menu Principal |::...");
+            System.out.println("1 --> Mostrar Inventario");
+            System.out.println("2 --> Agregar productos a carrito");
+            System.out.println("3 --> Mostrar Carrito");
+            System.out.println("4 --> Quitar productos de carrito");
+            System.out.println("5 --> Comprar carrito");
+            System.out.println("6 --> Mostrar lista de Compras");
+            System.out.println("7 --> Salir");
+            int opcMenu = GestorConsola.readIntFromKeyboard("Seleccione una opcion: ");
+            switch(opcMenu){
+                case 1: mostrarInventario(); break;
+                case 2: agregarProductosCarrito(carrito);break;
+                case 3: mostrarCarrito(carrito); break;
+                case 4: eliminarCarrito(carrito);  break;
+                case 5: comprarCarrito(carrito, miniAlx); 
+                        carrito = null; break;
+                case 6: miniAlx.imprimirCompras();break;
+                case 7: continuar = false; break;
+                default: System.out.println("Seleccion no valida");
+            }
+        }while(continuar);
 
         //generate and print ticket
             //tax calculation (IVA 16%)
@@ -33,37 +54,12 @@ public class PDVMain {
         //close session or to buy again
     }
 
-    public static void mainMenu(Carrito carrito, Tienda tienda) {
-        //Runtime.getRuntime().exec("cls");
-        System.out.println("...::| Menu Principal |::...");
-        System.out.println("1 --> Mostrar Inventario");
-        System.out.println("2 --> Agregar productos a carrito");
-        System.out.println("3 --> Mostrar Carrito");
-        System.out.println("4 --> Quitar productos de carrito");
-        System.out.println("5 --> Comprar carrito");
-        System.out.println("6 --> Mostrar lista de Compras");
-        System.out.println("7 --> Salir");
-        int opcMenu = GestorConsola.readIntFromKeyboard("Seleccione una opcion: ");
-        switch(opcMenu){
-            case 1: mostrarInventario(carrito,tienda); break;
-            case 2: agregarProductosCarrito(carrito,tienda);break;
-            case 3: mostrarCarrito(carrito,tienda); break;
-            case 4: eliminarCarrito(carrito,tienda);  break;
-            case 5: comprarCarrito(carrito, tienda); break;
-            case 6: tienda.imprimirCompras();
-                mainMenu(carrito,tienda);break;
-            case 7: break;
-            default: System.out.println("Seleccion no valida");
-                mainMenu(carrito,tienda);
-        }
-    }
-
-    private static void mostrarInventario(Carrito carrito,Tienda tienda) {
+    
+    private static void mostrarInventario() {
         System.out.println(Tienda.inventario);
-        mainMenu(carrito,tienda);
     }
 
-    static void agregarProductosCarrito(Carrito carrito, Tienda tienda){
+    static void agregarProductosCarrito(Carrito carrito){
         //add items to cart
         if(carrito == null)
             carrito = new Carrito();
@@ -72,46 +68,48 @@ public class PDVMain {
             int cant = GestorConsola.readIntFromKeyboard("Cuantos: ");
             carrito.agregarArticulo(sku, cant);
         }while(GestorConsola.askForContinue());
-        mainMenu(carrito,tienda);
     }
 
-    static void mostrarCarrito(Carrito carrito,Tienda tienda){
+    static void mostrarCarrito(Carrito carrito){
         if(carrito == null || carrito.getListaCompras().isEmpty()){
             System.out.println("No hay productos en el carrito");
         }else{
             System.out.print(carrito);
-        }
-        mainMenu(carrito,tienda);
+        };
     }
 
-    static void eliminarCarrito(Carrito carrito,Tienda tienda){
+    static void eliminarCarrito(Carrito carrito){
         //delete items from cart
         while(GestorConsola.askForDelete()){
             carrito.removerArticulo(
                     GestorConsola.redStringFromKeyboard("Que articulo deseas remover: "),
                     GestorConsola.readIntFromKeyboard("Cuantos ? : "));
         }
-        mainMenu(carrito,tienda);
     }
 
     static  void comprarCarrito(Carrito carrito, Tienda miniAlx){
         if (carrito == null){
             System.out.println("No hay productos en el carrito");
-            mainMenu(carrito,miniAlx);
+            return;
         }
         //purchase items from cart
         //generate order id
         int idCompra = miniAlx.comprarCarrito(carrito);
-
         miniAlx.buscar(idCompra).getTicket().imprimirTicket();
-
-        carrito = null;
-
-        mainMenu(carrito,miniAlx);
     }
 
     static void facturar(){
-
+        System.out.println("Seleccione una opcion");
+        System.out.println("1 --> Buscar cliente por RFC");
+        System.out.println("2 --> Registrar cliente");
+        int opcFacturar = GestorConsola.readIntFromKeyboard("Opcion: ");
+        switch(opcFacturar){
+            case 1: break;
+            case 2: break;
+            default : System.out.println("Seleccione una opcion valida");
+            facturar();
+            break;
+        }
     }
     
 }
